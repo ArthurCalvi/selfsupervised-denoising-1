8cbad3e163f98143a8de05344ff7584f9af130e3
CALVI <arthur.calvi@ens-paris-saclay.fr>
2022-01-15 22:40:36 +0100

    noise coeff correction

--- selfsupervised_denoising.py
+++ selfsupervised_denoising.py
@@ -692,7 +692,7 @@ def train(submit_config,
             else :
                 noisy_in_gpu = clean_in_split[gpu]
                 clean_in_gpu = clean_in_split[gpu]
-                noise_coeff = [float(p) / 255.0 for p in noise_style.replace('gauss', '', 1).split('_')][0]
+                _, noise_coeff = noisify(clean_in_gpu, noise_style)
             #MODIF end
 
             if pipeline == 'blindspot_mean':
